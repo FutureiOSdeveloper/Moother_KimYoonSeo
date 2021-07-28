a001ef77b8277f567a3d20c960ff673e3ebb847f
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-28 13:49:01 +0900

    [#3] Constants 정의

---
+++ Apple-Weather/Apple-Weather/Resources/Constants/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Apple-Weather
+//
+//  Created by 김윤서 on 2021/07/27.
+//
+
+struct Constants {
+    struct TableViewHeaders {
+        static let dailyWeather: String = "DailyWeatherHeaderView"
+    }
+    
+    struct TableViewCells {
+        static let temperature: String = "TemperatureTableViewCell"
+    }
+    
+    struct CollectionViewCells {
+        static let dailyWeather: String = "DailyWeatherCollectionViewCell"
+    }
+}
