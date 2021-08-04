bb23264d080cef5b380b2331720ed05ab72d0bc3
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-08-04 18:34:49 +0900

    [#12, #14] 기기 대응, location Stack View 구조 개선

--- Apple-Weather/Apple-Weather/Sources/ViewControllers/MainViewController.swift
+++ Apple-Weather/Apple-Weather/Sources/ViewControllers/MainViewController.swift
@@ -22,33 +22,68 @@ class MainViewController: UIViewController {
         $0.image = UIImage(named: "backImage")
     }
     
-    private let spacingView = UIView()
-    private let locationView = LocationView()
-    private let temperatureView = TemperatureView()
+    private let locationLabel = UILabel().then {
+        $0.text = "용인시"
+        $0.font = .systemFont(ofSize: 28)
+        $0.textColor = .white
+    }
+    
+    private let weatherLabel = UILabel().then {
+        $0.text = "한때 흐림"
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .white
+    }
+    
+    private let locationStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 5
+        $0.alignment = .center
+    }
+    
+    private let temperatureLabel = UILabel().then {
+        $0.text = "34"
+        $0.font = .systemFont(ofSize: 80, weight: .thin)
+        $0.textColor = .white
+    }
+    
+    private let highTemperatureLabel = UILabel().then {
+        $0.text = "최고:34"
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .white
+    }
+    
+    private let lowTemperatureLabel = UILabel().then {
+        $0.text = "최저:24"
+        $0.font = .systemFont(ofSize: 12)
+        $0.textColor = .white
+    }
+    
+    private let temperatureStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+    }
+    
+    private let locationViewTopConstraint: CGFloat = 50 * Constants.Ratio.iPhone12Pro
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initMainTableView()
-        layoutMainViewController()
+        setLayoutMainViewController()
     }
     
-    private func layoutMainViewController() {
-        view.addSubviews(backgroundImageView, locationView, temperatureView, mainTableView)
-
+    private func setLayoutMainViewController() {
+        view.addSubviews(backgroundImageView, locationStackView, mainTableView)
+        
+        locationStackView.addArrangedSubviews(locationLabel, weatherLabel, temperatureLabel, temperatureStackView)
+        temperatureStackView.addArrangedSubviews(highTemperatureLabel, lowTemperatureLabel)
+        
+        locationStackView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(locationViewTopConstraint)
+            $0.leading.trailing.equalToSuperview()
+        }
+       
         backgroundImageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
-        }
-        
-        locationView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(50)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(100)
-        }
-        
-        temperatureView.snp.makeConstraints {
-            $0.top.equalTo(locationView.snp.bottom)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(250)
         }
         
         mainTableView.snp.makeConstraints {
@@ -75,59 +110,58 @@ class MainViewController: UIViewController {
 extension MainViewController: UITableViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
-        let percentage = lastContentOffset / 292 * 4
-        let scrollRatio = abs(lastContentOffset / UIScreen.main.bounds.height)
+        let scrollRatio = abs(lastContentOffset / UIScreen.main.bounds.height) * 2.5
         
         guard let weekWeatherTableViewCell = mainTableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? WeekTableViewCell else { return }
         
         if lastContentOffset <= 0 {
+            locationStackView.snp.updateConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(max(locationViewTopConstraint, locationViewTopConstraint * (1 - scrollRatio)))
+            }
             weekWeatherTableViewCell.setEnabledScroll(isScrollEnabled: false)
-            temperatureView.setAlphaStackView(alpha: 1)
-            temperatureView.setAlphaTemperatureLabel(alpha: 1)
+            temperatureLabel.alpha = 1
+            temperatureStackView.alpha = 1
             scrollView.bounces = true
             
         } else if scrollRatio > 0 && scrollRatio < 0.04 {
             weekWeatherTableViewCell.setEnabledScroll(isScrollEnabled: true)
-            locationView.snp.updateConstraints {
-                $0.top.equalTo(view.safeAreaLayoutGuide).offset(50 * (1 - percentage))
+            locationStackView.snp.updateConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(locationViewTopConstraint * (1 - scrollRatio))
             }
-            temperatureView.setAlphaStackView(alpha: 1 - percentage * 2)
-            temperatureView.setAlphaTemperatureLabel(alpha: 1 - percentage)
-            
+            temperatureLabel.alpha = 1 - scrollRatio
+            temperatureStackView.alpha = 1 - scrollRatio * 2
             scrollView.bounces = true
             
-        } else if scrollRatio >= 0.04 && lastContentOffset < 0.3 {
+        } else if scrollRatio >= 0.04 && scrollRatio < 0.25 {
             weekWeatherTableViewCell.setEnabledScroll(isScrollEnabled: false)
-            locationView.snp.updateConstraints {
-                $0.top.equalTo(view.safeAreaLayoutGuide).offset(50 * (1 - percentage))
+            locationStackView.snp.updateConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(locationViewTopConstraint * (1 - scrollRatio))
             }
-            temperatureView.setAlphaStackView(alpha: 0)
-            temperatureView.setAlphaTemperatureLabel(alpha: 1 - percentage)
-            
+            temperatureLabel.alpha = 1 - scrollRatio * 2
+            temperatureStackView.alpha = 0
             scrollView.bounces = false
         } else {
+            locationStackView.snp.updateConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide).offset(max(5 * Constants.Ratio.iPhone12Pro, locationViewTopConstraint * (1 - scrollRatio)))
+            }
             weekWeatherTableViewCell.setEnabledScroll(isScrollEnabled: true)
-            temperatureView.setAlphaStackView(alpha: 0)
-            temperatureView.setAlphaTemperatureLabel(alpha: 0)
-            
+            temperatureLabel.alpha = 0
+            temperatureStackView.alpha = 0
             scrollView.bounces = false
         }
         
         lastContentOffset = scrollView.contentOffset.y
         
 //        print(UIScreen.main.bounds.height)
-        print(abs(lastContentOffset/UIScreen.main.bounds.height))
+        print(UIScreen.main.bounds.height)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-       return 530
+        return 530 * Constants.Ratio.iPhone12Pro
     }
     
-}
-
-extension MainViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+       return UIScreen.main.bounds.height / 2
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -143,21 +177,25 @@ extension MainViewController: UITableViewDataSource {
         return nil
     }
     
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 0
+     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+         return 0
+     }
+   
+}
+
+extension MainViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        return 1
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard  let cell = mainTableView.dequeueReusableCell(withIdentifier: Constants.TableViewCells.week, for: indexPath) as? WeekTableViewCell else { return UITableViewCell() }
         cell.selectionStyle = .none
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-       return UIScreen.main.bounds.height / 2
-    }
+
 }
