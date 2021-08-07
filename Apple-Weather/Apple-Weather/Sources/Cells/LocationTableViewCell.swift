5033aa62a17c97682bae8d55048682e42e7c4a9b
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-08-07 21:02:51 +0900

    [#16] LocationTableViewCell 레이아웃 수정

--- Apple-Weather/Apple-Weather/Sources/Cells/LocationTableViewCell.swift
+++ Apple-Weather/Apple-Weather/Sources/Cells/LocationTableViewCell.swift
@@ -14,26 +14,26 @@ class LocationTableViewCell: UITableViewCell {
     
     private let topLabel = UILabel().then {
         $0.text = "용인시"
-        $0.font = .systemFont(ofSize: 8)
+        $0.font = .systemFont(ofSize: 12)
         $0.textColor = .white
     }
     
     private let locationLabel = UILabel().then {
         $0.text = "나의 위치"
-        $0.font = .systemFont(ofSize: 20)
+        $0.font = .systemFont(ofSize: 25)
         $0.textColor = .white
     }
     
     private let locationStackView = UIStackView().then {
         $0.axis = .vertical
-        $0.spacing = 5
+//        $0.spacing = 5
         $0.alignment = .leading
         $0.distribution = .fillProportionally
     }
     
     private let temperatureLabel = UILabel().then {
         $0.text = "27"
-        $0.font = .systemFont(ofSize: 25)
+        $0.font = .systemFont(ofSize: 50, weight: .thin)
         $0.textColor = .white
     }
     
@@ -64,12 +64,14 @@ class LocationTableViewCell: UITableViewCell {
         
         locationStackView.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(Constants.Spacing.s20)
-            $0.centerY.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.height.equalTo(50)
         }
         
         temperatureLabel.snp.makeConstraints {
             $0.trailing.equalToSuperview().offset(-Constants.Spacing.s20)
-            $0.centerY.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.height.equalTo(50)
         }
     }
 
